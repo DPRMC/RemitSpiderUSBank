317200853fb4f2c637bc3eeda9876a69ae87c833
Michael Drennen <michaeldrennen74@gmail.com>
2022-03-21 14:35:42 -0600

    Setting up the framework.

---
+++ src/RemitSpiderUSBank.php
@@ -0,0 +1,10 @@
+<?php
+namespace DPRMC\RemitSpiderUSBank;
+
+class RemitSpiderUSBank {
+
+
+    public function __construct(string $user, string $pass){
+
+    }
+}
---
+++ tests/RemitSpiderUSBankTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+
+/**
+ * To run tests call:
+ * php ./vendor/phpunit/phpunit/phpunit --group=first
+ * Class BusinessDateTest
+ */
+class RemitSpiderUSBankTest extends TestCase {
+
+    /**
+     * @test
+     */
+    public function testHolder() {
+
+        $this->assertTrue(true);
+    }
+
+
+
+}
