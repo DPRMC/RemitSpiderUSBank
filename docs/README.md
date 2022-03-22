b36950e55e3d89d6a8f206fbda045e50937dca96
Michael Drennen <michaeldrennen74@gmail.com>
2022-03-21 19:10:51 -0600

    Next.

---
+++ docs/README.md
@@ -0,0 +1,3 @@
+# Remit Spider US Bank
+
+A spider meant to read remit data from US Bank.
--- src/RemitSpiderUSBank.php
+++ src/RemitSpiderUSBank.php
@@ -3,8 +3,21 @@ namespace DPRMC\RemitSpiderUSBank;
 
 class RemitSpiderUSBank {
 
+    const URL_LOGIN_FORM = 'https://gctinvestorreporting.bnymellon.com';
 
+    protected string $user;
+    protected string $pass;
+
+
+    /**
+     * @param string $user
+     * @param string $pass
+     */
     public function __construct(string $user, string $pass){
-
+        $this->user = $user;
+        $this->pass = $pass;
     }
+
+
+
 }
