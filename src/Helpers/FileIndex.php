bf004782162cfdb35bac230f5d8c8253eadb01fa
Michael Drennen <michaeldrennen74@gmail.com>
2022-04-22 16:55:24 -0600

    Parsing files.

--- src/Helpers/FileIndex.php
+++ src/Helpers/FileIndex.php
@@ -3,6 +3,7 @@
 namespace DPRMC\RemitSpiderUSBank\Helpers;
 
 
+use Carbon\Carbon;
 use DPRMC\RemitSpiderUSBank\RemitSpiderUSBank;
 use HeadlessChromium\Page;
 
@@ -18,10 +19,10 @@ class FileIndex {
     protected string $dealId;
 
 
-    /**
-     * Ex: https://trustinvestorreporting.usbank.com/TIR/public/deals/periodicReportHistory/1234/2/5678?extension=CSV
-     */
-    const BASE_FILE_URL = RemitSpiderUSBank::BASE_URL . '/TIR/public/deals/periodicReportHistory/';
+//    /**
+//     * Ex: https://trustinvestorreporting.usbank.com/TIR/public/deals/periodicReportHistory/1234/2/5678?extension=CSV
+//     */
+//    const BASE_FILE_URL = RemitSpiderUSBank::BASE_URL . '/TIR/public/deals/periodicReportHistory/';
 
 
     /**
@@ -52,6 +53,20 @@ class FileIndex {
     }
 
 
+    /**
+     * @param string $historyLinkSuffix
+     *
+     * @return array
+     * @throws \HeadlessChromium\Exception\CommunicationException
+     * @throws \HeadlessChromium\Exception\CommunicationException\CannotReadResponse
+     * @throws \HeadlessChromium\Exception\CommunicationException\InvalidResponse
+     * @throws \HeadlessChromium\Exception\CommunicationException\ResponseHasError
+     * @throws \HeadlessChromium\Exception\FilesystemException
+     * @throws \HeadlessChromium\Exception\NavigationExpired
+     * @throws \HeadlessChromium\Exception\NoResponseAvailable
+     * @throws \HeadlessChromium\Exception\OperationTimedOut
+     * @throws \HeadlessChromium\Exception\ScreenshotFailed
+     */
     public function get( string $historyLinkSuffix ): array {
 
         $this->_setDealId( $historyLinkSuffix );
@@ -66,104 +81,170 @@ class FileIndex {
         $this->Debug->_html( 'historic_files_page_' . urlencode( $historyLinkSuffix ) );
 
         $html = $this->Page->getHtml();
-        $dom = new \DOMDocument();
+        $dom  = new \DOMDocument();
         @$dom->loadHTML( $html );
-        //$element = $dom->getElementById( 'deal-document-results-table' );
-
-        $tableBody = $dom->getElementsByTagName( 'tbody' )->item(0);
-
-        $tableBodyChildren = $tableBody->childNodes;
-
-        print_r($tableBodyChildren);
-        flush();
-        die();
-
-        $tableRows = $dom->getElementsByTagName( 'tr' );
 
 
+        $anchors = $dom->getElementsByTagName( 'a' );
         /**
-         * @var \DOMElement $tr
+         * @var \DOMElement $anchor
          */
-        foreach( $tableRows as $tr):
-//            var_dump(get_class($tr)); flush(); die();
-            $class = $tr->getAttribute( 'class' );
+        foreach ( $anchors as $anchor ):
 
-            if('head' == empty($class)):
-                continue;
+            $href = $anchor->getAttribute( 'href' );
+            if ( $this->_isFileLink( $href ) ):
+
+                $fileTypeNode = $anchor->parentNode;
+                $fileType     = trim( $fileTypeNode->nodeValue );
+
+
+                /**
+                 * @var \DOMText $reportDateNode
+                 */
+                $reportDateNode = $fileTypeNode->previousSibling;
+                $reportDate     = Carbon::parse( trim( $reportDateNode->nodeValue ) );
+
+
+                /**
+                 * @var \DOMElement $garbageNode
+                 */
+                $garbageNode = $reportDateNode->previousSibling;
+
+
+                /**
+                 * @var \DOMElement $garbageNode
+                 */
+                $garbageNode = $garbageNode->previousSibling;
+
+
+                /**
+                 * @var \DOMElement $garbageNode
+                 */
+                $garbageNode = $garbageNode->previousSibling;
+                $reportName  = trim( $garbageNode->nodeValue );
+
+
+                /**
+                 * @var \DOMElement $garbageNode
+                 */
+//                $garbageNode = $garbageNode->previousSibling;
+//                var_dump(get_class($garbageNode)); flush();
+//                var_dump(trim($garbageNode->nodeValue)); flush();
+
+                var_dump( $fileType, $reportDate->toDateString(), $reportName );
+                flush();
+
+                die();
+
+                print('$reportNameNode class: ');
+                var_dump( get_class( $reportNameNode ) );
+                flush();
+                $reportName = trim( $reportNameNode->nodeValue );
+                $tagName    = trim( $reportNameNode->tagName );
+                print('tagname: ');
+                var_dump( $tagName );
+                print('$reportName value: ');
+                var_dump( $reportName );
+
+                $reportXNode = $reportNameNode->previousSibling;
+                $reportX     = trim( $reportXNode->nodeValue );
+                $tagName     = trim( $reportXNode->tagName );
+                print('tagname');
+                var_dump( $tagName );
+                print('$reportX value');
+                var_dump( $reportX );
+
+
+                $reportXNode = $reportXNode->previousSibling;
+                $reportX     = trim( $reportXNode->nodeValue );
+                $tagName     = trim( $reportXNode->tagName );
+                print('tagname');
+                var_dump( $tagName );
+                print('$reportX value');
+                var_dump( $reportX );
+
+
+                $reportXNode = $reportXNode->previousSibling;
+                $reportX     = trim( $reportXNode->nodeValue );
+                $tagName     = trim( $reportXNode->tagName );
+                print('tagname');
+                var_dump( $tagName );
+                print('$reportX value');
+                var_dump( $reportX );
+
+
+                flush();
+                die();
+
+                $fileLinks[] = $href;
             endif;
-
-
-
-
-
-            var_dump($class); flush();
         endforeach;
-
-        die();
-
-        var_dump($element); flush(); die();
-        foreach ( $elements as $element ):
-            $class = $element->getAttribute( 'class' );
-
-            // This is the one we want!
-            if ( 'periodic_report_2' == $class ):
-                $historyLinks[] = $element->getAttribute( 'href' );
-            endif;
-        endforeach;
-//
-//        $this->_cacheHistoryLinks( $historyLinks );
-//
 
         return $fileLinks;
     }
 
 
     /**
-     * @param array $newHistoryLinks
+     * @param array $newFileLinks
      *
      * @return void
      * @throws \Exception
      */
-//    protected function _cacheHistoryLinks( array $newHistoryLinks ): void {
-//
-//        // Init the array if it does not exist.
-//        if ( file_exists( $this->pathToHistoryLinks ) ):
-//            $jsonHistoryLinks = file_get_contents( $this->pathToHistoryLinks );
-//            $historyLinks     = json_decode( $jsonHistoryLinks, TRUE );
-//        else:
-//            $historyLinks = [];
-//        endif;
-//
-//
-//        // Init the Security Index of the array if it does not exist.
-//        if ( FALSE == array_key_exists( $this->dealId, $historyLinks ) ):
-//            $historyLinks[ $this->dealId ] = [];
-//        endif;
-//
-//        // Write all the new history links to the array.
-//        foreach ( $newHistoryLinks as $historyLink ):
-//            $myKey                                   = $this->_getMyUniqueId( $historyLink );
-//            $historyLinks[ $this->dealId ][ $myKey ] = $historyLink;
-//        endforeach;
-//
-//
-//        // Encode and save the array to the json file.
-//        $writeSuccess = file_put_contents( $this->pathToHistoryLinks,
-//                                           json_encode( $historyLinks ) );
-//        if ( FALSE === $writeSuccess ):
-//            throw new \Exception( "Unable to write US Bank Deal History Links to cache file: " . $this->pathToHistoryLinks );
-//        endif;
-//    }
+    protected function _cacheHistoryLinks( array $newFileLinks ): void {
+
+        // Init the array if it does not exist.
+        if ( file_exists( $this->pathToFileIndex ) ):
+            $jsonFileLinks = file_get_contents( $this->pathToFileIndex );
+            $fileLinks     = json_decode( $jsonFileLinks, TRUE );
+        else:
+            $fileLinks = [];
+        endif;
+
+
+        // Init the Security Index of the array if it does not exist.
+        if ( FALSE == array_key_exists( $this->dealId, $fileLinks ) ):
+            $fileLinks[ $this->dealId ] = [];
+        endif;
+
+        // Write all the new history links to the array.
+        foreach ( $newHistoryLinks as $historyLink ):
+            $myKey                                = $this->_getMyUniqueId( $historyLink );
+            $fileLinks[ $this->dealId ][ $myKey ] = $historyLink;
+        endforeach;
+
+
+        // Encode and save the array to the json file.
+        $writeSuccess = file_put_contents( $this->pathToHistoryLinks,
+                                           json_encode( $fileLinks ) );
+        if ( FALSE === $writeSuccess ):
+            throw new \Exception( "Unable to write US Bank Deal History Links to cache file: " . $this->pathToHistoryLinks );
+        endif;
+    }
 
 
     /**
-     * Helper function. This returns an MD5 hash used as a unique identifier (array index) for each history link.
+     * Helper function. This returns an MD5 hash used as a unique identifier (array index) for each file link.
      *
-     * @param string $historyLink
+     * @param string $href
      *
      * @return string
      */
-    protected function _getMyUniqueId( string $historyLink ): string {
-        return md5( $historyLink );
+    protected function _getMyUniqueId( string $href ): string {
+        return md5( $href );
+    }
+
+
+    /**
+     * @param string $href
+     *
+     * @return bool
+     */
+    protected function _isFileLink( string $href ): bool {
+        $prefix = '/TIR/public/deals/populateReportDocument/';
+        if ( FALSE === stripos( $href, $prefix ) ):
+            return FALSE;
+        endif;
+
+        return TRUE;
     }
 }
