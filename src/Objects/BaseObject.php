531728168f6d2f851e4eacce39d65144e3692e4a
Michael Drennen <michaeldrennen74@gmail.com>
2022-05-03 14:35:55 -0600

    Refactor.

--- src/Objects/BaseObject.php
+++ src/Objects/BaseObject.php
@@ -9,14 +9,30 @@ use DPRMC\RemitSpiderUSBank\Collectors\BaseData;
 abstract class BaseObject {
 
     private array $_data;
+    private string $_timezone;
 
     public Carbon $addedAt;
-    public Carbon $lastPulled;
+    public Carbon $lastPulledAt;
+    public string $timezone;
 
-    public function __construct( array $data ) {
+    public function __construct( array $data, string $timezone) {
         $this->_data = $data;
+        $this->_timezone = $timezone;
 
-        $this->addedAt    = $data[ BaseData::ADDED_AT ];
-        $this->lastPulled = $data[ BaseData::LAST_PULLED ];
+        $this->addedAt      = $data[ BaseData::ADDED_AT ];
+        $this->lastPulledAt = $data[ BaseData::LAST_PULLED ];
+    }
+
+
+    /**
+     * @return bool
+     */
+    public function pulledInTheLastDay(): bool {
+        $now = Carbon::now($this->_timezone);
+        $diffInSeconds = $now->diffInSeconds($this->lastPulledAt);
+        if(86400 > $diffInSeconds):
+            return true;
+        endif;
+        return false;
     }
 }
--- src/Objects/Deal.php
+++ src/Objects/Deal.php
@@ -2,10 +2,17 @@
 
 namespace DPRMC\RemitSpiderUSBank\Objects;
 
+use Carbon\Carbon;
+use DPRMC\RemitSpiderUSBank\RemitSpiderUSBank;
+use HeadlessChromium\Page;
 
-class Portfolio extends BaseObject {
+
+
+class Deal extends BaseObject {
 
     public function __construct( array $data, string $timezone ) {
         parent::__construct( $data, $timezone );
     }
+
+
 }
