dd195aef3eb8d534382c35f6d83831d497587463
Michael Drennen <michaeldrennen74@gmail.com>
2022-05-05 10:34:01 -0600

    Refactor.

--- src/Objects/File.php
+++ src/Objects/File.php
@@ -2,7 +2,6 @@
 
 namespace DPRMC\RemitSpiderUSBank\Objects;
 
-
 use DPRMC\RemitSpiderUSBank\Collectors\FileIndex;
 
 class File extends BaseObject {
@@ -10,8 +9,4 @@ class File extends BaseObject {
     public function __construct( array $data, string $timezone, string $pathToCache ) {
         parent::__construct( $data, $timezone, $pathToCache );
     }
-
-
-
-
 }
