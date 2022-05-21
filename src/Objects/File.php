da78018088cce19f8b079365fc78babd73e7b94e
Michael Drennen <michaeldrennen74@gmail.com>
2022-05-21 14:16:53 -0600

    Refactor.

--- src/Objects/File.php
+++ src/Objects/File.php
@@ -28,6 +28,13 @@ class File extends BaseObject {
         return $this->_data[ 'type' ];
     }
 
+    public function getCleanType(): string {
+        $type = $this->getType();
+        $type = strtolower( $type );
+        $type = str_replace( ' ', '-', $type );
+        return $type;
+    }
+
     public function getDate(): Carbon {
         return Carbon::parse( $this->_data[ 'date' ], $this->getTimezone() );
     }
@@ -36,23 +43,19 @@ class File extends BaseObject {
         return $this->_data[ 'name' ];
     }
 
+    public function getCleanName(): string {
+        $name = $this->getName();
+        $name = strtolower( $name );
+        $name = str_replace( ' ', '-', $name );
+        $name = str_replace( '_', '-', $name );
+        $name = str_replace( '--', '-', $name );
+        return $name;
+    }
+
     public function getHref(): string {
         return $this->_data[ 'href' ];
     }
 
-    /**
-     * HREF: /TIR/public/deals/populateReportDocument/47370585/CSV
-     * RETURNS: csv
-     *
-     * @return string
-     */
-    public function getFileExtension(): string {
-        $href          = $this->getHref();
-        $hrefParts     = explode( '/', $href );
-        $fileExtension = end( $hrefParts );
-        $fileExtension = strtolower( $fileExtension );
-        return $fileExtension;
-    }
 
     public function getLink(): string {
         return RemitSpiderUSBank::BASE_URL . $this->_data[ 'href' ];
@@ -66,10 +69,5 @@ class File extends BaseObject {
         return Carbon::parse( $this->_data[ 'addedAt' ], $this->getTimezone() );
     }
 
-    public function getCleanType(): string {
-        $type = $this->getType();
-        $type = strtolower( $type );
-        $type = str_replace( ' ', '-', $type );
-        return $type;
-    }
+
 }
