2df336efadac15ad13ee1af00f15348d7b9a1579
Michael Drennen <michaeldrennen74@gmail.com>
2022-05-21 13:46:10 -0600

    Refactor.

--- src/Objects/File.php
+++ src/Objects/File.php
@@ -25,30 +25,37 @@ class File extends BaseObject {
     }
 
     public function getType(): string {
-        return $this->_data['type'];
+        return $this->_data[ 'type' ];
     }
 
     public function getDate(): Carbon {
-        return Carbon::parse($this->_data['date'], $this->timezone);
+        return Carbon::parse( $this->_data[ 'date' ], $this->timezone );
     }
 
     public function getName(): string {
-        return $this->_data['name'];
+        return $this->_data[ 'name' ];
     }
 
     public function getHref(): string {
-        return $this->_data['href'];
+        return $this->_data[ 'href' ];
     }
 
     public function getLink(): string {
-        return RemitSpiderUSBank::BASE_URL .  $this->_data['href'];
+        return RemitSpiderUSBank::BASE_URL . $this->_data[ 'href' ];
     }
 
     public function getChildrenLastPulled(): string {
-        return Carbon::parse($this->_data['childrenLastPulled'], $this->timezone);
+        return Carbon::parse( $this->_data[ 'childrenLastPulled' ], $this->timezone );
     }
 
     public function getAddedAt(): string {
-        return Carbon::parse($this->_data['addedAt'], $this->timezone);
+        return Carbon::parse( $this->_data[ 'addedAt' ], $this->timezone );
+    }
+
+    public function getCleanType(): string {
+        $type = $this->getType();
+        $type = strtolower( $type );
+        $type = str_replace( ' ', '-', $type );
+        return $type;
     }
 }
