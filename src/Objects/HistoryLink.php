5445df1faa78fe4caddb7a0a717a7d2d1d54dcd5
Michael Drennen <michaeldrennen74@gmail.com>
2022-06-01 14:55:09 -0600

    Nullable.

--- src/Objects/HistoryLink.php
+++ src/Objects/HistoryLink.php
@@ -7,13 +7,20 @@ use DPRMC\RemitSpiderUSBank\Collectors\HistoryLinks;
 class HistoryLink extends BaseObject {
 
     protected string $portfolioId;
-    protected string $dealId;
+
+    /**
+     * @var string|mixed|null
+     */
+    protected ?string $dealId;
 
     public function __construct( array $data, string $timezone, string $pathToCache, string $portfolioId ) {
-        parent::__construct( $data, $timezone,  $pathToCache );
+        parent::__construct( $data, $timezone, $pathToCache );
 
         $this->portfolioId = $portfolioId;
-        $this->dealId = $data[HistoryLinks::DEAL_ID];
+
+        $this->dealId = $data[ HistoryLinks::DEAL_ID ] ?? NULL;
+
+
     }
 
 
@@ -22,14 +29,15 @@ class HistoryLink extends BaseObject {
      * @throws \Exception
      */
     public function getLink(): string {
-        if(isset($this->_data[HistoryLinks::LINK])):
-            return $this->_data[HistoryLinks::LINK];
+        if ( isset( $this->_data[ HistoryLinks::LINK ] ) ):
+            return $this->_data[ HistoryLinks::LINK ];
         endif;
-        throw new \Exception("Key " . HistoryLinks::LINK . " not found in _data for " . self::class);
+        throw new \Exception( "Key " . HistoryLinks::LINK . " not found in _data for " . self::class );
     }
 
     /**
      * A simple getter.
+     *
      * @return string
      */
     public function getPortfolioId(): string {
@@ -39,6 +47,7 @@ class HistoryLink extends BaseObject {
 
     /**
      * A simple getter.
+     *
      * @return string
      */
     public function getDealId(): string {
