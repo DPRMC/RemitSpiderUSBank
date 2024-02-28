47221701fdedc4090af97330b624741b6051b274
Michael Drennen <michaeldrennen74@gmail.com>
2024-02-28 08:58:22 -0500

    Updated custom exception constructor.

--- src/Exceptions/Exception404Returned.php
+++ src/Exceptions/Exception404Returned.php
@@ -10,4 +10,11 @@ use HeadlessChromium\Page;
  */
 class Exception404Returned extends \Exception {
 
+    public string $url = '';
+
+    public function __construct( string $message = "", int $code = 0, ?\Throwable $previous = NULL, string $url = '' ) {
+        parent::__construct( $message, $code, $previous );
+
+        $this->url = $url;
+    }
 }
